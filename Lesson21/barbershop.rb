d7813d0844be93a002115f95292fe433b3c5e2ef
Mifka186 <mifka186@gmail.com>
2024-04-14 13:30:50 +0300

    bags is ficsed

--- Lesson21/barbershop.rb
+++ Lesson21/barbershop.rb
@@ -7,7 +7,7 @@ end
 post '/' do
     @user_name = params[:user_name]
     @phone = params[:phone]
-    @date and time[:date_time]
+    @date_time = params[:date_time]
 
     @title = "Thank you!"
     @message = "Dear #{@user_name}, we'll be waiting for you at #{@date_time}"
