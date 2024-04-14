a41914644381847e85c89559d31dad6a46477857
Mifka186 <mifka186@gmail.com>
2024-04-14 13:06:16 +0300

    доработано

--- Lesson21/barbershop.rb
+++ Lesson21/barbershop.rb
@@ -8,4 +8,9 @@ post '/' do
     @user_name = params[:user_name]
     @phone = params[:phone]
     @date and time[:date_time]
+
+    @title = "Thank you!"
+    @message = "Dear #{@user_name}, we'll be waiting for you at #{@date_time}"
+
+    erb :message
 end
