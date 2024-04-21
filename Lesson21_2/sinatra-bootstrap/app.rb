a31c4fa882439236aac0a4bf9e0126d8a8b8abcb
Mifka186 <mifka186@gmail.com>
2024-04-21 14:39:20 +0300

    добавлен вход по логину и паролю

--- Lesson21_2/sinatra-bootstrap/app.rb
+++ Lesson21_2/sinatra-bootstrap/app.rb
@@ -1,6 +1,23 @@
 require 'rubygems'
 require 'sinatra'
 
+set :public_folder, __dir__ + '/public'
+
+get '/' do
+    erb :index
+end
+
+post '/' do
+    @login = params[:aaa]
+    @password = params[:bbb]
+
+    if @login == "aaa" and @password == "bbb"
+        erb :welcome
+    else
+        erb :accessdenied
+    end
+end
+
 configure do
   enable :sessions
 end
